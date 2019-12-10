1972954bc610470285d14a073ee5de61d57056a5
FredyCcahuana <fr3dy.ccahuana@gmail.com>
2019-12-10 04:35:26 -0500

    gg

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import mongoose,{ connect } from 'mongoose'
 
 export async function startConnection() {
     //para realizar la conecion a la base de datos
-    const db = await connect('mongodb://mongodb+srv://fredyccahuana:FCPfisi2020@cluster0-v5dvs.mongodb.net/test?retryWrites=true&w=majority',{
+    const db = await connect('mongodb+srv://fredyccahuana:FCPfisi2020@cluster0-v5dvs.mongodb.net/test?retryWrites=true&w=majority',{
         //para que no brinde error por consola
         useNewUrlParser: true,
         useUnifiedTopology: true,
